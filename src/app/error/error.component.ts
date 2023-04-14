58115a17a1859587b0c1bbebd1b3c7237ba60a84
snhkn <senihaakin@hotmail.com>
2023-04-14 18:08:33 +0300

    Add Route to error component

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.css']
 })
-export class ErrorComponent {
+export class ErrorComponent implements OnInit{
 
+  errorMessage = 'An Error Occured! Contact support at *** - ***'
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
 }
