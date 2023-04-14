23c7e8a7c20c437f66349d79f0ccedd08e723875
snhkn <senihaakin@hotmail.com>
2023-04-14 16:18:53 +0300

    Setting up login component, click event binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit{
 
+  username = 'defaultUserName'
+  password = ''
+
   constructor(){}
 
   ngOnInit(): void {
     
+  }
+
+  handleLogin() {
+    console.log(this.username);
   }
 
 }
