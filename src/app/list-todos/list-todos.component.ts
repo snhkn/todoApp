d7c77a7de9660b7aed311e8ac30ca4fd476db5f2
snhkn <senihaakin@hotmail.com>
2023-04-15 22:19:48 +0300

    Create list of todos

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
   styleUrls: ['./list-todos.component.css']
 })
-export class ListTodosComponent {
+export class ListTodosComponent implements OnInit {
+ 
+  todos = [
+    {id: 1, description : 'Learn Angular'},
+    {id: 2, description :'Clean the House'},
+    {id: 3, description : 'Study German'}
+  ]
 
+  // todo = {
+  //   id : 1,
+  //   description : 'Learn to Dance'
+  // }
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
 }
