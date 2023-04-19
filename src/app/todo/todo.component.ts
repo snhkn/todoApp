258eeda237fc3d98585069765565c469aebd1f74
snhkn <senihaakin@hotmail.com>
2023-04-19 12:10:05 +0300

    Connect frontend with todo service

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../service/data/todo-data.service';
+import { Todo } from '../list-todos/list-todos.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
 
+  id!: number;
+  todo!: Todo;
+
+  constructor(
+    private todoService: TodoDataService,
+    private route :ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.id  = this.route.snapshot.params['id'];
+    this.todoService.retrieveTodo('defaultuser', this.id).subscribe(
+      response => this.todo = response
+    );
+  }
 }
