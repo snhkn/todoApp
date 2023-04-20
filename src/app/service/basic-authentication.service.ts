0d0a2f981bb50ed5c334ddbd045950ffc904e774
snhkn <senihaakin@hotmail.com>
2023-04-20 21:24:16 +0300

    feat: Create Angular Basic Authentication Service

---
+++ src/app/service/basic-authentication.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BasicAuthenticationService {
+
+  constructor(private http : HttpClient) { }
+
+  // http://localhost:8080/basicauth
+  executeAuthenticationService(username:string, password : string){
+    let basicAuthHeaderString = 'Basic '+ window.btoa(username+':'+password);
+    
+    let header = new HttpHeaders({
+      Authorization : basicAuthHeaderString
+    });
+    return this.http.get<AuthenticationBean>(`http://localhost:8080/basicauth`, 
+      {headers : header}).pipe(
+        map(
+          data => {
+            sessionStorage.setItem('authenticatedUser',username);
+            //sessionStorage.setItem('token',basicAuthHeaderString);
+            return data;
+          }
+        )
+      );
+    //console.log("Execute Basic Authentication Bean Service");
+  }
+
+  getAuthenticatedUser(){
+   return sessionStorage.getItem('authenticatedUser')
+  }
+
+  getAuthenticatedToken() : any {
+    if(this.getAuthenticatedUser())
+      return sessionStorage.getItem('token')
+  }
+
+  isUserLoggedIn(){
+    let user = sessionStorage.getItem('authenticatedUser')
+    return !(user === null)
+  }
+  
+  logout(){
+    sessionStorage.removeItem('authenticatedUser')
+    sessionStorage.removeItem('token')
+  }
+}
+export class AuthenticationBean{
+  constructor(public message : string){}
+}
+
+
