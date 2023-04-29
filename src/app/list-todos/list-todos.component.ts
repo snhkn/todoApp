2cd017e3691ffbe593aa31c3e35fdb87d25f9ba2
snhkn <senihaakin@hotmail.com>
2023-04-29 22:10:09 +0300

    feat: Add timer feature to count the time spent on a task

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -23,7 +23,12 @@ export class ListTodosComponent implements OnInit {
 
   todos: Todo[] = [];
   message: string = '';
-  username!: any;
+  username: any;
+  selectedTodo!: Todo;
+  isStarted: boolean = false;
+  startTime!: Date;
+  interval: any;
+  timeElapsed: number = 0;
 
   constructor(
     private todoService: TodoDataService,
@@ -62,5 +67,31 @@ export class ListTodosComponent implements OnInit {
 
   addTodo(): void {
     this.router.navigate(['todos', -1])
+  }
+  
+  toggleButton(todo: Todo) {
+ 
+    if (this.isStarted) {
+      // stop the timer
+      clearInterval(this.interval);
+      this.isStarted = false;
+    } else {
+      // start the timer
+      this.isStarted = true;
+      this.selectedTodo = todo;
+      this.startTime = new Date();
+      this.interval = setInterval(() => {
+        this.timeElapsed = new Date().getTime() - this.startTime.getTime();
+      }, 1000);
+      console.log(this.timeElapsed);
+    }
+  }
+
+  formatTime(ms: number): string {
+    const seconds = Math.floor(ms / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+  
+    return `${hours}h ${minutes % 60}m ${seconds % 60}s`;
   }
 }
